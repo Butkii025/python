ce9c4ecc8e6185c0320871154a88484166bedec1
Butkii <butkii099@gmail.com>
2025-12-13 20:59:40 +0530

    update

---
+++ day3.py
@@ -0,0 +1,21 @@
+# hey it`s my third day of learning python, hope I finish it earlier.
+# let`s learn loop program in pyhton, today
+
+i=0  #iterator
+while (i<=5):
+    print("vijju")
+    i+=1
+
+#print no. 0 to 10
+
+a=0
+while(a<=10):
+    print(a)
+    a+=1
+
+#print in reverse 10 to 0
+
+r=10
+while(r>=0):
+    print(r)
+    r-=1
