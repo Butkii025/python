3005588b8b1d441985034994d9622c052fa5c4b7
Butkii <butkii099@gmail.com>
2025-12-30 17:34:17 +0530

    initial commit

---
+++ oops pillars/inheritance.py
@@ -0,0 +1,19 @@
+# inheritance
+
+class Employee:
+    start_time="10am"
+    end_time="6pm"
+
+class Teacher(Employee):
+    def __init__(self,subject):
+        self.subjet = subject
+
+t1= Teacher("maths")
+
+print(f"t1 have {t1.subjet} as subject with start time {t1.start_time} and end time {t1.end_time} .")
+
+# print(t1.subjet, t1.start_time, t1.end_time)
+
+
+
+
