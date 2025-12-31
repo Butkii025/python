eb39ba781dee1e0a06f35b353daf1f04c79b1261
Butkii <butkii099@gmail.com>
2025-12-31 12:06:11 +0530

    updated

--- oops pillars/polymorphism.py
+++ oops pillars/polymorphism.py
@@ -1,4 +1,6 @@
 # polymorphism : is a multiple function with same name having similar work
+
+
 # overloading, overriding, overwritting
 
 class Employee:
@@ -11,3 +13,28 @@ class Teacher(Employee):     # here uses the same class and create an case of (o
 
 t1= Teacher()
 t1.get_designation()
+
+print("And here is example of Duck Typing 👇.")
+
+
+# Duct typing
+
+class Teacher():
+    def get_designation(self):
+        print(" designation= Teacher")
+
+
+class Accountant():
+    def get_designation(self):
+        print(" designation= Accountant")
+
+
+t1= Teacher()
+t1.get_designation()
+
+
+acc1=Accountant()
+acc1.get_designation()
+
+
+
